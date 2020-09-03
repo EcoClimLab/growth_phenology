9c521ce6cce2f122cd6cf3329c4085c0b2919241
camerondow35 <cdow@gmu.edu>
2020-09-03 12:14:45 -0400

    Update Climwin_Cameron.R

--- RScripts/Climwin_Cameron.R
+++ RScripts/Climwin_Cameron.R
@@ -1,5 +1,6 @@
 #Climwin attempt
 library(climwin)
+library(readr)
 #NCDC_NOAA Front Royal station data
 climate <- read_csv("climate data/NCDC_NOAA_precip_temp.csv")
 climate <- climate[complete.cases(climate$TMAX),]
@@ -7,7 +8,7 @@ climate <- climate[complete.cases(climate$DATE),]
 
 #The data containing the biological responses for testing
 Wood_pheno_table <- read_csv("Data/Wood_pheno_table_V4.csv") #Master datafrmae containing 20%, 50%, and 75% growth milestones
-twentyfive <- subset(Wood_pheno_table, perc == .25 & wood_type == "diffuse-porous") # subset to only 25%
+twentyfive <- subset(Wood_pheno_table, perc == .25 & wood_type == "ring porous") # subset to only 25%
 
 #We would want to test all three percentages (25, 50, 75) but I dont know how to.
 #I would also like to test how $Max_rate, $Max_rate_DOY, and $tot (total growth) are correlated with climate
@@ -19,22 +20,20 @@ for(i in c(2011:2019)){ #Assigns dates in the proper format for Climwin analysis
   biodata <- rbind(biodata, df)
 }
 
+round(mean(biodata$DOY))
 
-#biodata <- biodata[biodata$date %in% climate$DATE,] #Removes any biodata where climate data is missing. Is this necessary?
 
-#trythis <- subset(biodata, year != 2011)
 MassWin <- slidingwin(xvar = list(Temp = climate$TMAX),
                       cdate = climate$DATE,
                       bdate = biodata$date,
                       baseline = lm(DOY ~ 1, data = biodata),
                       cinterval = "day",
-                      range = c(153, 0),
+                      range = c(113, 0),
                       type = "relative",
                       stat = "mean",
                       func = "lin",
                       cmissing = "method2")
 
-#Best window is 37-0 days before 25% DOY?
 
 MassRand <- randwin(repeats = 5,
                     xvar = list(Temp = climate$TMAX),
@@ -42,10 +41,34 @@ MassRand <- randwin(repeats = 5,
                     bdate = biodata$date,
                     baseline = lm(DOY ~ 1, data = biodata),
                     cinterval = "day",
-                    range = c(50, 0), #make range the reference day to January 1st
+                    range = c(113, 0), #make range the reference day to January 1st
                     type = "relative", #make reference day the mean DOY
                     stat = "mean",
                     func = "lin",
                     cmissing = "method2")
 
 pvalue(dataset = MassWin[[1]]$Dataset, datasetrand = MassRand[[1]], metric = "AIC")
+
+MassSingle <- singlewin(xvar = list(Temp = climate$TMAX),
+                        cdate = climate$DATE,
+                        bdate = biodata$date,
+                        baseline = lm(DOY ~ 1, data = biodata),
+                        cinterval = "day",
+                        range = c(36, 0),
+                        type = "relative",
+                        stat = "mean",
+                        func = "lin",
+                        cmissing = "method2")
+MassOutput <- MassWin[[1]][["Dataset"]]
+plotdelta(dataset = MassOutput)
+plotweights(dataset = MassOutput)
+plotbetas(dataset = MassOutput)
+plotwin(dataset = MassOutput)
+plotbest(dataset = MassOutput,
+         bestmodel = MassSingle$BestModel,
+         bestmodeldata = MassSingle$BestModelData)
+
+plotall(dataset = MassOutput,
+        datasetrand = MassRand,
+        bestmodel = MassSingle$BestModel,
+        bestmodeldata = MassSingle$BestModelData)
