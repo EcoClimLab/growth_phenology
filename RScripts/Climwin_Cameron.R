b874e09414ce809b0e2eddfb224515c08cf211a5
camerondow35 <cdow@gmu.edu>
2020-09-16 12:34:18 -0400

    Climwin results
    PNG's of climwin results / updated climwin script to loop through wood type > percentage > range feb 1 or mar 1

--- RScripts/Climwin_Cameron.R
+++ RScripts/Climwin_Cameron.R
@@ -1,14 +1,35 @@
 #Climwin attempt
 library(climwin)
 library(readr)
+library(lubridate)
+library(tidyverse)
 #NCDC_NOAA Front Royal station data
-climate <- read_csv("climate data/NCDC_NOAA_precip_temp.csv")
+#climate <- read_csv("climate data/NCDC_NOAA_precip_temp.csv")
+#climate <- climate[complete.cases(climate$TMAX),]
+#climate <- climate[complete.cases(climate$DATE),]
+
+#SCBI met tower
+climate <- read_csv("climate data/SCBI_mettower_data_sensor2.csv", col_names = FALSE)
+colnames(climate) <- c("year", "month", "day", "precip", "TMAX", "TMIN")
+climate[climate$TMAX == -99.9,] <- NA
 climate <- climate[complete.cases(climate$TMAX),]
-climate <- climate[complete.cases(climate$DATE),]
+
+climate$DATE <- paste(climate$day, climate$month, climate$year, sep = "/")
+climate$DATE <- strptime(as.character(climate$DATE), format = "%d/%m/%Y")
+climate$DATE <- format(climate$DATE, "%d/%m/%Y")
 
 #The data containing the biological responses for testing
 Wood_pheno_table <- read_csv("Data/Wood_pheno_table_V4.csv") #Master datafrmae containing 20%, 50%, and 75% growth milestones
-twentyfive <- subset(Wood_pheno_table, perc == .25 & wood_type == "ring porous") # subset to only 25%
+
+rangedates <- data.frame("feb1", 30)
+rangedates2 <- data.frame("mar1", 59)
+names(rangedates) <- names(rangedates2)
+rangedates <- rbind(rangedates, rangedates2)
+colnames(rangedates) <- c("day", "doy")
+
+for(w in unique(Wood_pheno_table$wood_type)){
+  for(j in unique(Wood_pheno_table$perc)){
+twentyfive <- subset(Wood_pheno_table, perc == j & wood_type == w) # subset to only 25%
 
 #We would want to test all three percentages (25, 50, 75) but I dont know how to.
 #I would also like to test how $Max_rate, $Max_rate_DOY, and $tot (total growth) are correlated with climate
@@ -20,16 +41,23 @@ for(i in c(2011:2019)){ #Assigns dates in the proper format for Climwin analysis
   biodata <- rbind(biodata, df)
 }
 
-round(mean(biodata$DOY))
+refdateround <- round(mean(biodata$DOY))
 
+#climatecheck <- climate[c(1:121),]
+#biodata <- biodata[!(biodata$year == 2011),]
+for (k in rangedates$doy) {
+
+refdate <- data.frame(round(mean(biodata$DOY)))
+refdate$round.mean.biodata.DOY.. <- as.Date(refdate$round.mean.biodata.DOY.., origin = paste0("2011-01-01"))
+refdate <- separate(refdate, "round.mean.biodata.DOY..", c("Year", "Month", "Day"), sep = "-")
 
 MassWin <- slidingwin(xvar = list(Temp = climate$TMAX),
                       cdate = climate$DATE,
                       bdate = biodata$date,
                       baseline = lm(DOY ~ 1, data = biodata),
                       cinterval = "day",
-                      range = c(113, 0),
-                      type = "relative",
+                      range = c((refdateround-k), 0), #feb 1st
+                      type = "absolute", refday = c(refdate$Day, refdate$Month),
                       stat = "mean",
                       func = "lin",
                       cmissing = "method2")
@@ -41,12 +69,11 @@ MassRand <- randwin(repeats = 5,
                     bdate = biodata$date,
                     baseline = lm(DOY ~ 1, data = biodata),
                     cinterval = "day",
-                    range = c(113, 0), #make range the reference day to January 1st
-                    type = "relative", #make reference day the mean DOY
+                    range = c((refdateround-k), 0),
+                    type = "absolute", refday = c(refdate$Day,refdate$Month),  #make reference day the mean DOY
                     stat = "mean",
                     func = "lin",
                     cmissing = "method2")
-
 pvalue(dataset = MassWin[[1]]$Dataset, datasetrand = MassRand[[1]], metric = "AIC")
 
 MassSingle <- singlewin(xvar = list(Temp = climate$TMAX),
@@ -54,21 +81,29 @@ MassSingle <- singlewin(xvar = list(Temp = climate$TMAX),
                         bdate = biodata$date,
                         baseline = lm(DOY ~ 1, data = biodata),
                         cinterval = "day",
-                        range = c(36, 0),
-                        type = "relative",
+                        range = c(MassWin[[1]][["Dataset"]][[1,2]], MassWin[[1]][["Dataset"]][[1,3]]),
+                        type = "absolute", refday = c(refdate$Day,refdate$Month),
                         stat = "mean",
                         func = "lin",
                         cmissing = "method2")
-MassOutput <- MassWin[[1]][["Dataset"]]
-plotdelta(dataset = MassOutput)
-plotweights(dataset = MassOutput)
-plotbetas(dataset = MassOutput)
-plotwin(dataset = MassOutput)
-plotbest(dataset = MassOutput,
-         bestmodel = MassSingle$BestModel,
-         bestmodeldata = MassSingle$BestModelData)
 
-plotall(dataset = MassOutput,
+
+MassOutput <- MassWin[[1]][["Dataset"]]
+MassRand <- MassRand[[1]]
+#plotdelta(dataset = MassOutput)
+#plotweights(dataset = MassOutput)
+#plothist(dataset = MassOutput, datasetrand = MassRand)
+#plotbetas(dataset = MassOutput)
+#plotwin(dataset = MassOutput)
+#plotbest(dataset = MassOutput,
+#         bestmodel = MassSingle$BestModel,
+#         bestmodeldata = MassSingle$BestModelData)
+
+png(filename = paste("SCBI", "mettower",w, k, j, ".png", sep = "_"), width = 10, height = 8, units = "in", res = 300)
+plotalloutput <- plotall(dataset = MassOutput,
         datasetrand = MassRand,
         bestmodel = MassSingle$BestModel,
         bestmodeldata = MassSingle$BestModelData)
+
+dev.off()
+}}}
