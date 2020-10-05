abf40c55b15650118703aa804a211d8da738f2d5
rudeboybert <albert.ys.kim@gmail.com>
2020-10-05 08:03:10 -0400

    Manually created credible interval plots

--- RScripts/bert/test.R
+++ RScripts/bert/test.R
@@ -1,4 +1,4 @@
-# 2020/9/15 -------
+# 2020/9/16, 2020/10/5 -------
 # Testing plotting credible intervals from http://mjskay.github.io/tidybayes/
 library(rstanarm)
 library(tidybayes)
@@ -10,18 +10,17 @@ library(modelr)
 
 
 
-# Visualize relationship between DOY and marchmeans -----------------------
+# Visualize relationship between DOY and marchmeans ----------------------------
 #
-# Run wood_phenology_bert.R lines 1-96 first
+# Run wood_phenology_analysis_bert.R lines 1-96 first
 #
 set.seed(76)
-random_tags <- Wood_pheno_table$tag %>%
-  unique() %>%
-  sample(size = 20)
+
+random_tags <- Wood_pheno_table %>% pull(tag) %>% unique() %>% sample(200)
 
 temp_data <- Wood_pheno_table %>%
   mutate(marchmean = marchmean - mean(marchmean)) %>%
-  filter(tag %in% random_tags) %>%
+  # filter(tag %in% random_tags) %>%
   select(tag, year, perc, DOY, marchmean) %>%
   arrange(tag, year, perc)
 
@@ -32,14 +31,14 @@ ggplot(temp_data, aes(x = marchmean, y = DOY, col = perc)) +
   geom_point() +
   facet_wrap(~perc) +
   geom_smooth(method = "lm", se = FALSE) +
-  geom_vline(xintercept = 0, linetype = "dashed")
-ggplotly()
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  labs(title = "All trees", x = "Mean-recentered march mean temperature")
 
 
 
 
 
-# Fit SIMPLE multivariate model using marchmeans -----------------------
+# Fit multivariate model -------------------------------------------------------
 joint_model_marchmeans <- stan_mvmer(
   formula = list(
     # DOY_25 ~ marchmean + (1|sp) + (1|tag),
@@ -55,34 +54,36 @@ joint_model_marchmeans <- stan_mvmer(
   # Once we feel good about our results, increase these values. The code will
   # take longer to run however
   chains = 2,
-  iter = 5000
+  iter = 2000
 )
-save(joint_model_marchmeans, file = "results/2020-09-16_preliminary_credible_intervals_rstanarm_model.RData")
+# save(joint_model_marchmeans, file = "results/2020-09-16_preliminary_credible_intervals_rstanarm_model.RData")
 
 # Hack bayesian regression table:
-joint_model_marchmeans %>%
+bayesian_regression_table <- joint_model_marchmeans %>%
   summary() %>%
   as_tibble(rownames = "coefficient") %>%
   select(coefficient, mean, `2.5%`, `97.5%`) %>%
   filter(!str_detect(coefficient, "Sigma")) %>%
   filter(str_detect(coefficient, "(Intercept)") | str_detect(coefficient, "marchmean"))
+bayesian_regression_table
 
 # Compare with standard lm output
-lm(DOY ~ perc + marchmean:perc, data = temp_data) %>%
+frequentist_regression_table <- lm(DOY ~ perc + marchmean:perc, data = temp_data) %>%
   tidy()
+frequentist_regression_table
 
 
 
 
 
-# Work with posterior draws -----------------------
+# Posterior draws --------------------------------------------------------------
 # Reference: http://mjskay.github.io/tidybayes/articles/tidy-rstanarm.html
 # Used to identify fixed effects of interest:
 joint_model_marchmeans %>%
   get_variables() %>%
   dput()
 
-# Extract draws from posterior distributions
+# Extract posterior draws
 posterior_draws <- joint_model_marchmeans %>%
   gather_draws(
     # This is a hack:
@@ -96,7 +97,10 @@ posterior_draws <- joint_model_marchmeans %>%
 
 
 
-# Extract draws corresponding to fixed effects
+
+
+# Fixed effects posterior draws ------------------------------------------------
+# Executive summary: Posterior regression parameters look good
 fixed_effects <- posterior_draws %>%
   filter(.variable != "b") %>%
   ungroup() %>%
@@ -111,15 +115,23 @@ fixed_effects <- posterior_draws %>%
     )
   ) %>%
   pivot_wider(names_from = coefficient, values_from = .value)
+fixed_effects
 
-# Posterior means of all fixed effects parameters. Compare to hack regression table above
+# Posterior means of all fixed effects parameters
 posterior_lines <- fixed_effects %>%
   group_by(perc) %>%
   summarize(`(Intercept)` = mean(`(Intercept)`), marchmean = mean(marchmean))
+posterior_lines
+
+# Identical to Bayesian regression table! Yay!
+bayesian_regression_table
 
 
 
-# Extract draws corresponding to random effects
+
+
+# Extract draws corresponding to random effects --------------------------------
+# Executive summary:
 random_effects <- posterior_draws %>%
   filter(.variable == "b") %>%
   ungroup() %>%
@@ -134,37 +146,56 @@ random_effects <- posterior_draws %>%
   ) %>%
   mutate(tag = str_replace_all(group, "tag:", "")) %>%
   select(-.variable, -group)
+random_effects
 
-# For each covariable combination, extract fitted value
+
+# Extract predicted values for each data point
+# ISSUE: Predicted values look off
 predictions <- temp_data %>%
-  add_predicted_draws(joint_model_marchmeans) %>%
+  add_predicted_draws(joint_model_marchmeans)
+
+predictions %>%
+  group_by(perc) %>%
+  summarize(mean_observed = mean(DOY), mean_predicted = mean(.prediction))
+
+
+# Manually compute predicted values instead of using add_predicted_draws() -----
+predictions <- predictions %>%
   mutate(.predictions_new = 0)
 
-for(i in 1:nrow(temp_data)){
+for(i in i:nrow(temp_data)){
   temp_data_i <- temp_data %>%
     slice(i)
+  tag_i <- temp_data_i$tag
+  year_i <- temp_data_i$year
+  perc_i <- temp_data_i$perc
+  marchmean_i <- temp_data_i$marchmean
 
+  # Get all fixed effects draws
   fixed_effects_i <- fixed_effects %>%
-    filter(perc == temp_data_i$perc)
+    filter(perc == perc_i)
 
+  # Get all random effects draws
   random_effects_i <- random_effects %>%
-    filter(tag == temp_data_i$tag, perc == temp_data_i$perc)
+    filter(perc == perc_i, tag == tag_i)
 
-  DOY_hat <- random_effects_i$.value +
+  # Compute predicted values
+  index_i <- predictions$tag == tag_i &
+    predictions$year == year_i &
+    predictions$perc == perc_i
+
+  predictions[index_i, "predictions_new"] <- random_effects_i$.value +
     fixed_effects_i$`(Intercept)` +
-    fixed_effects_i$marchmean * temp_data_i$marchmean
-
-  index <- predictions$tag == temp_data_i$tag &
-    predictions$year == temp_data_i$year &
-    predictions$perc == temp_data_i$perc
-  predictions[index,]$.predictions_new <- DOY_hat
+    fixed_effects_i$marchmean * marchmean_i
 }
 
 
 # File reprex on this: mean_DOY_hat is not working right for stanmvreg objects
 predictions %>%
+  ungroup() %>%
+  slice_head(n = 600*2000) %>%
   group_by(perc) %>%
-  summarize(mean_DOY = mean(DOY), mean_DOY_hat = mean(.prediction), mean_DOY_hat_new = mean(.predictions_new))
+  summarize(mean_observed = mean(DOY), mean_predicted = mean(.prediction), mean_predicted_new = mean(predictions_new))
 
 
 
